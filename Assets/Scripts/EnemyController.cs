d18dee59883813d664960aabf5dbd25d0b24eb92
LurdVoldemort <drwhostinson@icloud.com>
2023-09-30 00:10:37 -0700

    Done basic enemy movement
    Finshed the prelimenary enemy movement system - enemy bee lines to player if the player is not in the bubble (simulated by pressing space to enter/exit bubble as player).

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+// using System.Numerics; //do I need this? other controllers don't have it and it makes new Vector2 throw an error
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -9,30 +10,23 @@ public class EnemyController : MonoBehaviour
     [SerializeField] PlayerDummyController player; // will work later, grants access to isPlayerInBubble
     [SerializeField] private int EnemyHealth;
     [SerializeField] private int AttackDamage;
-    [SerializeField] private int EnemySpeed;
-
-    private Rigidbody2D enemyRigidBody2D;
-
-    // Start is called before the first frame update
-
-    void Awake() {
-        enemyRigidBody2D = gameObject.AddComponent<Rigidbody2D>() as Rigidbody2D;
-        
-    }
+    [SerializeField] private float EnemySpeed;
 
     void Start() {
-        player = GameObject.FindObjectOfType<PlayerDummyController>(); // init player
     }
 
-    // Update is called once per frame
     void Update() {
-        
+        if (player.playerInBubble) {
+            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, EnemySpeed * Time.deltaTime);
+        }
     }
 }
 
 
-// health
-// if player is in bubble, give velocity in direction of player
+// health - ability to be hit by player
+// if player is not in bubble, give velocity in direction of player DONE
 // if within range of player, swing and deal damage
 // Could: animation if enemy is touching bubble
+// onDeath - drop orb and delete prefab
+// spawn function
 // 
