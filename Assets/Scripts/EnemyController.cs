1cb78f945491c39314b5cd224777276d46b6b5ce
LurdVoldemort <drwhostinson@icloud.com>
2023-09-30 14:40:12 -0700

    Attacks player when in range
    Mostly just made it so when the enemy is in range of player, stops moving and starts attacking every x seconds, dealing x damage. Also started OnDeath() but couldnt figure how to instantiate orb prefab.

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -3,21 +3,69 @@ using System.Collections.Generic;
 // using System.Numerics; //do I need this? other controllers don't have it and it makes new Vector2 throw an error
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
 
     [SerializeField] PlayerDummyController player; // will work later, grants access to isPlayerInBubble
-    [SerializeField] private int EnemyHealth;
-    [SerializeField] private int AttackDamage;
-    [SerializeField] private float EnemySpeed;
+    [SerializeField] private int enemyHealth;
+    [SerializeField] private int attackDamage;
+    [SerializeField] private float enemySpeed = 1.0f;
+    [SerializeField] private float enemyRange = 1.0f;
+    [SerializeField] private float enemyAttackDelay = 2.5f;
+    [SerializeField] GameObject dummyOrb;
+    private float timePassed = 0f; // for attack delay
+
+    void IsPlayerInBubble() {
+        if (player.playerInBubble && ! IsEnemyInRange()) {
+            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, enemySpeed * Time.deltaTime);
+        }
+    }
+
+    bool IsEnemyInRange() {
+        float distanceBetweenEnemyAndPlayer = Vector2.Distance(gameObject.transform.position, player.transform.position);
+        if (distanceBetweenEnemyAndPlayer <= enemyRange) { // is player in range of enemy
+            if (player.playerHealth > 0) {
+                EnemyAttack();
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    void EnemyAttack() {
+        timePassed += Time.deltaTime;
+        if (timePassed > enemyAttackDelay) {
+            // play animation
+            Debug.Log("hit!");
+            player.playerHealth -= 1;
+            timePassed = 0f;
+        }
+    }
+
+    void OnDeath() { // how do I properly instantiate orb at postion of enemy
+        // Instantiate(dummyOrb);
+        // dummyOrb.transform = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
+        Destroy(gameObject);
+    }
+
+    // private void OnTriggerEnter2D(Collider2D other) { //COULD: animation for enemy trying to smash bubble
+    //     if (other.tag == "bubble") {
+    //         //play animation
+    //     }
+    // }
 
     void Start() {
+
     }
 
     void Update() {
-        if (player.playerInBubble) {
-            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, EnemySpeed * Time.deltaTime);
+        IsPlayerInBubble();
+
+        if (Input.GetKeyDown(KeyCode.LeftShift)) { // just to test dying
+            OnDeath();
         }
     }
 }
@@ -25,8 +73,8 @@ public class EnemyController : MonoBehaviour
 
 // health - ability to be hit by player
 // if player is not in bubble, give velocity in direction of player DONE
-// if within range of player, swing and deal damage
-// Could: animation if enemy is touching bubble
+// if within range of player, swing and deal damage DONE (need animation though)
+// Could: animation if enemy is touching bubble DONE (again need animation)
 // onDeath - drop orb and delete prefab
 // spawn function
 // 
