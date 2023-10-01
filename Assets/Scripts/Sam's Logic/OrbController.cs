25f01a81409cc1f616596f3676b007b9587d52b7
LurdVoldemort <drwhostinson@icloud.com>
2023-09-30 21:08:00 -0700

    Working on enemy movement and player picking up orb
    lot of things not working here - will fix tomorrow.

--- Assets/Scripts/Sam's Logic/OrbController.cs
+++ Assets/Scripts/Sam's Logic/OrbController.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 
 public class OrbController : MonoBehaviour
 {
+
+    [SerializeField] Rigidbody2D player;
+    [SerializeField] private float pickupSpeed;
+
     void Start()
     {
         Debug.Log("Orb is here");
         // Animation / particles
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        
+        if (other.tag == "Player") {
+            while (transform.position != player.transform.position) {
+                transform.position = Vector2.MoveTowards(transform.position, player.transform.position, pickupSpeed * Time.deltaTime); // instantly teleports when it should slowly move
+                // give player orb and play animation
+            }
+            //Destroy(gameObject);
+        }
+        
     }
 
     void Update()
