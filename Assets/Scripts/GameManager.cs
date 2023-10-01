e1935756b8e1ad3a4919e6239784da8605fab4b9
Brendan Wadey <62085968+Broondoon@users.noreply.github.com>
2023-10-01 15:20:25 -0600

    Adding the Game Manager
    Slotted it in.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,21 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Plop this guy 
+
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] GameObject droppedOrb;
-    [SerializeField] private float pickupSpeed;
+    //[SerializeField] GameObject droppedOrb; // probably don't need a full thing
+    // [SerializeField] private float pickupSpeed;
+    
     [SerializeField] GameObject player;
 
-    
 
-    void Start()
-    {
-        
+    public static GameManager instance { get; private set;}
+
+    private void Awake() {
+        if (instance != null) {
+            Debug.LogError("Found more than one Game Manager in the scene.");
+        }
+        instance = this;
     }
 
-    void Update()
-    {
-        
-    }
+
 }
