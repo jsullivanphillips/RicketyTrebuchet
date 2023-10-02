d22b92595e5f8e0ba93d6465d24a9c739fd31183
Jamie Sullivan-Phillips <47346975+jsullivanphillips@users.noreply.github.com>
2023-10-01 19:51:43 -0700

    In Level scene: Added game managaer, added SFX to the playerController script when placing lights and casting the beam, and added some ambience

--- Assets/Scripts/GameLogic/GameManager.cs
+++ Assets/Scripts/GameLogic/GameManager.cs
@@ -7,16 +7,16 @@ public class GameManager : MonoBehaviour
     [SerializeField] GameObject droppedOrb;
     [SerializeField] private float pickupSpeed;
     [SerializeField] GameObject player;
-
-    
+    [SerializeField] Player_Controller_test playerScript;
+  
 
     void Start()
     {
-        
-    }
-
-    void Update()
-    {
+        if(AudioManager.Singleton != null)
+        {
+            AudioManager.Singleton.Play("Ambience");
+            AudioManager.Singleton.Play("SpookySong");
+        }
         
     }
 }
--- Assets/Scripts/GameLogic/Player_Controller_test.cs
+++ Assets/Scripts/GameLogic/Player_Controller_test.cs
@@ -50,6 +50,7 @@ public class Player_Controller_test : MonoBehaviour
 
         if (Input.GetMouseButtonDown(0) && Chrystal_spawn_count > 0 && Can_spawn_Crystal == true)
         {
+            AudioManager.Singleton.Play("ButtonClick");
             Chrystal_spawn_count -= 1;
             Cooldown_timer_Crystal = 0;
             Can_spawn_Crystal = false;
@@ -62,10 +63,17 @@ public class Player_Controller_test : MonoBehaviour
 
         if (Input.GetMouseButton(1))
         {
+            
             new_blast = Instantiate(sparkle, wand.position, wand.rotation); ;
 
             new_blast.GetComponent<Rigidbody2D>().velocity = wand.rotation * Vector2.right*28;
         }
+        if(Input.GetMouseButtonDown(1))
+        {
+            AudioManager.Singleton.Play("CastStart");
+            AudioManager.Singleton.Play("Casting");
+        }
+
 
         if (difference.x >= 0 && !facingRight)
         { // mouse is on right side of player
